516eaab2f4195deb881a69904d2d61200d7d1be1
Yishuai Li <yishuai@upenn.edu>
2021-05-02 15:24:52 -0400

    Jexp to JSON

--- theories/Common.v
+++ theories/Common.v
@@ -6,6 +6,8 @@ From ExtLib Require Export
 From Coq Require Export
      Basics
      List.
+Export
+  ListNotations.
 Open Scope program_scope.
 
 Definition get' {K V} (eqb : K -> K -> bool) (k : K) : list (K * V) -> option V :=
@@ -13,3 +15,13 @@ Definition get' {K V} (eqb : K -> K -> bool) (k : K) : list (K * V) -> option V 
 
 Definition get {K V} `{Dec_Eq K} : K -> list (K * V) -> option V :=
   get' (fun k k' => k = k'?).
+
+Definition map_snd {K V U} (f : V -> U) : list (K * V) -> list (K * U) :=
+  map (fun kv => let (k, v) := kv : K * V in (k, f v)).
+
+Fixpoint pick_some {A} (l : list (option A)) : list A :=
+  match l with
+  | [] => []
+  | Some a :: l' => a :: pick_some l'
+  | None   :: l' =>     pick_some l'
+  end.
--- theories/Jexp.v
+++ theories/Jexp.v
@@ -1,10 +1,15 @@
+From Parsec Require Export
+     Parser.
 From ExtLib Require Export
      Extras.
+From Coq Require Export
+     ssr.ssrfun.
 From AsyncTest Require Export
      Trace
      Common.
 Export
   FunNotation.
+Open Scope parser_scope.
 
 Inductive jpath :=
   Jpath__This
@@ -15,22 +20,43 @@ Inductive jexp :=
   Jexp__Const  : json                             -> jexp
 | Jexp__Array  : list jexp                        -> jexp
 | Jexp__Object : list (string * jexp)             -> jexp
-| Jexp__Ref    : labelT -> jpath -> (traceT -> json) -> jexp.
+| Jexp__Ref    : labelT -> jpath                   -> jexp.
 
-Fixpoint jget (p : jpath) (j : json) : option json :=
+Fixpoint jget' (fn : nat -> json -> option json)
+         (fs : string -> json -> option json)
+         (p : jpath) (j : json) : option json :=
   match p with
   | Jpath__This => Some j
-  | Jpath__Array n p' => get_nth n j >>= jget p'
-  | Jpath__Object s p' => get_json' s j >>= jget p'
+  | Jpath__Array n p' => fn n j >>= jget' fn fs p'
+  | Jpath__Object s p' => fs s j >>= jget' fn fs p'
   end.
 
-Fixpoint jexp_to_json (t : traceT) (e : jexp) : json :=
+Example jget_strong : jpath -> json -> option json := jget' get_nth get_json'.
+
+Definition nth_weak (n : nat) (j : json) : option json :=
+  if j is JSON__Array l then get_nth (min n $ pred $ length l) j else None.
+
+Definition jget_weak : jpath -> json -> option json := jget' nth_weak get_json'.
+
+Example tget_strong (l : labelT) (p : jpath) (t : traceT) : json :=
+  odflt JSON__Null $ get l t >>= jget_strong p.
+
+Definition tget_weak' (jget : jpath -> json -> option json)
+           (l : labelT) (p : jpath) (t : traceT) : json :=
+  odflt (last (pick_some $ map (jget p ∘ snd) t) JSON__Null) $
+        get l t >>= jget p.
+
+Definition tget_weak : labelT -> jpath -> traceT -> json := tget_weak' jget_weak.
+
+Fixpoint jexp_to_json' (tget : labelT -> jpath -> traceT -> json)
+         (t : traceT) (e : jexp) : json :=
   match e with
   | Jexp__Const  j => j
-  | Jexp__Array  l => JSON__Array  $ map (jexp_to_json t) l
-  | Jexp__Object m =>
-    JSON__Object $ map (fun se => let (s, e) := se : string * jexp in
-                             (s, jexp_to_json t e)) m
-  | Jexp__Ref l p f => if get l t >>= jget p is Some j then j
-                    else f t
+  | Jexp__Array  l => JSON__Array  $ map     (jexp_to_json' tget t) l
+  | Jexp__Object m => JSON__Object $ map_snd (jexp_to_json' tget t) m
+  | Jexp__Ref  l p => tget l p t
   end.
+
+Example jexp_to_json_strong : traceT -> jexp -> json := jexp_to_json' tget_strong.
+
+Definition jexp_to_json_weak : traceT -> jexp -> json := jexp_to_json' tget_weak.
